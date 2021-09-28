f92639cede72b0a3fb91581e8e3ded953e6cdcb1
Thomas Lauer <tjlauer36579@gmail.com>
2021-09-28 18:20:05 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _20210907_MavPASS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Pasta myPasta1 = new Pasta("new name",5);
+            Pasta myPasta2 = new Pasta();
+            Pasta myPasta3 = new Pasta();
+
+            Console.WriteLine(myPasta1.Name);
+            Console.WriteLine(myPasta1.Name);
+            Console.WriteLine(myPasta1.TimeToCook);
+            myPasta1.TimeToCook = 3;
+            Console.WriteLine(myPasta1.TimeToCook);
+            Console.WriteLine(myPasta1.Name);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pasta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20210907_MavPASS
+{
+    public class Pasta
+    {
+        // Variables
+        private string name = "n/a";
+        private double timeToCook = -1;
+
+
+        // Properties
+        public string Name { 
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public double TimeToCook
+        {
+            get { return this.timeToCook; }
+            set { this.timeToCook = value; }
+        }
+
+        // Constructors
+        public Pasta()
+        {
+
+        }
+
+        public Pasta(string aName, double aTimeToCook )
+        {
+            this.Name = aName;
+            this.TimeToCook = aTimeToCook;
+        }
+    }
+}
